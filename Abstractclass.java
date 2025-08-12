90665680890451ebba77ac8e4a74a9c37379b5aa
Nwabisa2005 <bedanwabisa9@gmail.com>
2025-08-12 10:56:57 +0000

    Add files via upload

---
+++ Abstractclass.java
@@ -0,0 +1,23 @@
+public class Abstractclass {
+    protected string driverName;
+    protected double speed;
+    protected double finePayable;
+
+    // Constructor
+    public Fine(string driveName, double speed){
+        this.driverName = driveName;
+        this.speed = speed;
+        CalculateFine();
+    }
+    //GET METHODS
+
+
+    public string getDriverName() {
+         driverName;
+         public double GetSpeed()= speed;
+         public double GetFinePayable() = finePayable;
+
+         //
+    }
+}
+}
